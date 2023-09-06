2ab1f5f816fdb57e72792d7e1964ca492ecd662c
camili <camili.costa07@gmail.com>
2023-09-06 14:45:12 -0300

    adicionar e finalizar

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite seu primeiro nome:");
+string nome = Console.ReadLine()!;
+Console.Write(nome);
+Console.WriteLine();
+Console.Write("Digite seu sobrenome:");
+string sobrenome = Console.ReadLine()!;
+Console.Write(sobrenome);
+Console.WriteLine();
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
+
+
